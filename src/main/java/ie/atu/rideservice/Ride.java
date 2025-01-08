279582e3e82909469d7ef108891bd1ff8e604ad6
G00400498@atu.ie <g00400498@atu.ie>
2025-01-08 14:33:04 +0000

    added validation to ride entity

--- src/main/java/ie/atu/rideservice/Ride.java
+++ src/main/java/ie/atu/rideservice/Ride.java
@@ -1,23 +1,31 @@
 package ie.atu.rideservice;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class Ride {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "User ID cannot be null")
     private Long userId;
+
+    @NotBlank(message = "Pickup location is required")
     private String pickupLocation;
+
+    @NotBlank(message = "Destination location is required")
     private String destinationLocation;
+
+    @NotBlank(message = "Status cannot be null")
     private String status;
 }
