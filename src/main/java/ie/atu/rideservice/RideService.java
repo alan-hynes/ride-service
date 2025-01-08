c399b5949f6df135c9a413c3926325f9a83ec534
G00400498@atu.ie <g00400498@atu.ie>
2025-01-08 15:01:11 +0000

    amended rideservice and tested the class

--- src/main/java/ie/atu/rideservice/RideService.java
+++ src/main/java/ie/atu/rideservice/RideService.java
@@ -1,42 +1,47 @@
 package ie.atu.rideservice;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class RideService {
 
     private final RideRepository rideRepository;
 
-    public RideService(RideRepository rideRepository) {
-        this.rideRepository = rideRepository;
-    }
-
-    public List<Ride> getAllRides() {
-        return rideRepository.findAll();
-    }
-
+    @Transactional
     public Ride createRide(Ride ride) {
         return rideRepository.save(ride);
     }
 
     public Ride getRideById(Long id) {
         return rideRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Ride not found with id: " + id));
+                .orElseThrow(() -> new ResourceNotFoundException("Ride not found with ID: " + id));
     }
 
+    public List<Ride> getAllRides() {
+        return rideRepository.findAll();
+    }
+
+    public List<Ride> getRidesByUserId(Long userId) {
+        return rideRepository.findByUserId(userId);
+    }
+
+    @Transactional
     public Ride updateRide(Long id, Ride rideDetails) {
-        return rideRepository.findById(id).map(ride -> {
-            ride.setPickupLocation(rideDetails.getPickupLocation());
-            ride.setDestinationLocation(rideDetails.getDestinationLocation());
-            ride.setStatus(rideDetails.getStatus());
-            ride.setUserId(rideDetails.getUserId());
-            return rideRepository.save(ride);
-        }).orElseThrow(() -> new ResourceNotFoundException("Ride not found with id: " + id));
+        Ride ride = getRideById(id);
+        ride.setPickupLocation(rideDetails.getPickupLocation());
+        ride.setDestinationLocation(rideDetails.getDestinationLocation());
+        ride.setStatus(rideDetails.getStatus());
+        return rideRepository.save(ride);
     }
 
+    @Transactional
     public void deleteRide(Long id) {
-        rideRepository.deleteById(id);
+        Ride ride = getRideById(id);
+        rideRepository.delete(ride);
     }
 }
